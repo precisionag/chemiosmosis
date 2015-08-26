6d3f5fb63380790748e0490658811664e53575e7
Chris Tucker <precisionag@yahoo.com>
2015-08-25 22:11:26 -0500

    Updated and removed some files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825123337) do
+ActiveRecord::Schema.define(version: 20150826013825) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
